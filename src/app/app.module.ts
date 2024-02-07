a82d45cee23de18c500f0e16c1f5bbf7c7f66bb3
AYLazcano <0370102@cegepmontpetit.ca>
2024-02-06 20:32:40 -0500

    FCT Importation des Modules FormsModule  et HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// Importation du module FormsModule 
+import { FormsModule } from '@angular/forms';
+// Importation du module HttpClientModule 
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Importation du module FormsModule 
+    FormsModule,
+    // Importation du module HttpClientModule 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
